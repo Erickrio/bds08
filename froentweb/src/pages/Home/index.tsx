d7a826ccd02165519ee5df77db983156b1210d62
Erick Monteiro <monteiro.ck@hotmail.com>
2023-01-04 10:06:42 -0300

    Home, Routes

---
+++ froentweb/src/pages/Home/index.tsx
@@ -0,0 +1,14 @@
+import './styles.css'
+
+const Home = () => {
+  return (
+   <div className="home-container">
+      <h1>Desafio Github API</h1>
+      <p> DevSuperior - Escola de programação</p>
+       <button className="btn btn-primary btn-lg start-button">Começar</button>
+   </div>
+  );
+
+}
+
+export default Home;
