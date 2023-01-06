fc0b4528378dab3d9ad9b8f7cbc6f4e64d69dd69
Erick Monteiro <monteiro.ck@hotmail.com>
2023-01-05 21:56:44 -0300

    finishi modals styles and begin others

--- froentweb/src/pages/GitSearch/index.tsx
+++ froentweb/src/pages/GitSearch/index.tsx
@@ -1,25 +1,31 @@
-import ResultCard from '../../components/ResultCard';
-import './styles.css'
+import './styles.css';
+
 
 const GitSearch = () => {
   return (
-    <div className="git-search-container">
+    <><div className="git-search-container">
       <div className="container search-container">
-      <h1>Encontre um perfil Github</h1>
+        <h1>Encontre um perfil Github</h1>
         <form>
           <div className="form-container">
             <input
               type="text"
               className="search-input"
-              placeholder="Usuário Github"
-                />
+              placeholder="Usuário Github" />
             <button type="submit" className="btn btn-primary search-button">
               Encontrar
-            </button> 
+            </button>
           </div>
         </form>
       </div>
     </div>
+      <>
+        <div className="container info-container">
+           <div className="container img-container">
+            <p>Imagem</p>
+           </div>
+        </div>
+      </></>
   );
 };
 
